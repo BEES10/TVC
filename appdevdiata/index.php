d074df6dc09de926848fd85beb0ce56657f3c918
YurishaShi <166886755+YurishaShi@users.noreply.github.com>
2024-04-12 19:21:30 -0700

    Add files via upload

---
+++ appdevdiata/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>R. It's what's happening / R</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/all
+    <style>
+        body {
+            background-color: #ffffff;
+            height: 100vh;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .happening-later {
+            position: absolute;
+            top: 80px;
+            right: 65px;
+            font-size: 70px;
+            font-weight: bold;
+        }
+        .join-tomorrow {
+            position: absolute;
+            top: 210px;
+            right: 350px;
+            font-size: 35px;
+            font-weight: bold;
+        }
+        .sign-up-boogle{
+            border: 1px solid rgba(108, 117, 125, 0.3);
+            color: #000000;
+            padding: 8px 16px;
+            margin-top: 10px;
+            position: absolute;
+            bottom: 330px;
+            right: 300px;
+            border-radius: 20px;
+            width: 300px;
+            background-color: #ffffff;
+            font-size: 15px;
+        }
+        .sign-up-boogle:hover {
+            background-color: rgba(255, 182, 193, 0.3);
+            color : #000000;
+            border: 1px solid rgba(108, 117, 125, 0.3);
+        }
+        .sign-up-grape{
+            border: 1px solid rgba(108, 117, 125, 0.3);
+            color: #000000;
+            padding: 8px 16px;
+            margin-top: 10px;
+            position: absolute;
+            bottom: 280px;
+            right: 300px;
+            border-radius: 20px;
+            width: 300px;
+            background-color: #ffffff;
+            font-size: 15px;
+            font-weight: bold;
+        }
+        .sign-up-grape:hover {
+            background-color: rgba(203, 153, 255, 0.3);
+            color: #000000;
+            border: 1px solid rgba(108, 117, 125, 0.3);
+        }
+
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="happening-later">Happening later</div>
+        <div class="join-tomorrow">Join tomorrow.</div>
+        <button type="button" class="btn btn-primary sign-up-boogle">Sign up with Boogle</button>
+        <button type="button" class="btn btn-secondary sign-up-grape">Sign up with Grape</button>
+        </div>
+</body>
+</html>
